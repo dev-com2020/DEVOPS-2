1f3dff58d972efda4589b9814e25582578002194
devcom <kwp@tlen.pl>
2024-06-18 15:49:00 +0200

    dodanie testów 1

--- test.py
+++ test.py
@@ -1,9 +1,36 @@
-osoby = [1, 2, 3, 4, 5]
-
-print(osoby)
+import unittest
 
 
-if osoby:
-    print("lista jest ok")
+def do_something():
+    return "coś robię..."
 
 
+def addition(a, b):
+    return a + b
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_something(self):
+        result = do_something()
+        assert result == "coś robię"
+
+    def test_main(self):
+        result = addition(3,2)
+        assert result == 5
+
+    def test_one(self):
+        pass
+
+    def test_two(self):
+        pass
+
+class MySecondTestCase(unittest.TestCase):
+    def test_3(self):
+        pass
+
+    def test_4(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
